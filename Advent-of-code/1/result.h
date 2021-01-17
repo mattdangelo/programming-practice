dad8ea0b934a0fb598ea378420a3a056e12e4dc3
matt <matthew.dangelo6@gmail.com>
2021-01-17 23:14:16 +1030

    First exercise

---
+++ Advent-of-code/1/result.h
@@ -0,0 +1,15 @@
+//
+// Created by Matt on 17/01/2021.
+//
+
+#ifndef ADVENT_OF_CODE_RESULT_H
+#define ADVENT_OF_CODE_RESULT_H
+
+struct result {
+    bool success;
+    int first;
+    int second;
+    result(bool, int, int);
+};
+
+#endif //ADVENT_OF_CODE_RESULT_H
