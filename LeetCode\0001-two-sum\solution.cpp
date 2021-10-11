314650d87d2f52d996c2095357402b99e0db3a82
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-10-11 10:59:14 +0000

    Sync LeetCode submission Runtime - 878 ms (5.05%), Memory - 10.1 MB (99.98%)

--- LeetCode\0001-two-sum\solution.cpp
+++ LeetCode\0001-two-sum\solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    for(int i=0;i<nums.size();i++) {
-        for(int j=i+1;j<nums.size();j++) {
-            if(nums.at(j) == target - nums.at(i)) {
-                std::vector<int> indices;
-                indices.push_back(i);
-                indices.push_back(j);
-                return indices;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size();
+        for(int i=0;i<size;i++) {
+            for(int j=i+1;j<size;j++) {
+                if(nums.at(j) == target - nums.at(i)) {
+                    std::vector<int> indicies;
+                    indicies.push_back(i);
+                    indicies.push_back(j);
+                    return indicies;
+                }
             }
         }
+        std::vector<int> error;
+        return error;
     }
-
-    std::vector<int> error = { INT32_MAX, INT32_MAX };
-    return error;
-}
 };
