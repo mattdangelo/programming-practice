ad6be451ad0e53039bbbf6eaa33e19829d008994
matt <matthew.dangelo6@gmail.com>
2021-10-23 12:29:31 +1030

    Include LeetCode problems

---
+++ LeetCode/TwoSum/C++/cmake-build-debug/CMakeFiles/TwoSum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TwoSum.dir/main.cpp.o"
+  "TwoSum"
+  "TwoSum.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TwoSum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Advent of Code
 https://adventofcode.com/
+
+LeetCode
+https://leetcode.com/Astradus/
