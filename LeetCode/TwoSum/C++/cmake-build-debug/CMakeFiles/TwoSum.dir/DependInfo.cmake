eaef675e18030117c3cfbc6319937f5c6d1efb05
matt <matthew.dangelo6@gmail.com>
2021-10-23 12:42:21 +1030

    Add TwoSum

--- LeetCode/TwoSum/C++/cmake-build-debug/CMakeFiles/TwoSum.dir/DependInfo.cmake
+++ LeetCode/TwoSum/C++/cmake-build-debug/CMakeFiles/TwoSum.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/mnt/e/Drive/Projects/ProgrammingPractice/LeetCode/TwoSum/main.cpp" "/mnt/e/Drive/Projects/ProgrammingPractice/LeetCode/TwoSum/cmake-build-debug/CMakeFiles/TwoSum.dir/main.cpp.o"
+  "/mnt/e/Drive/Projects/ProgrammingPractice/LeetCode/TwoSum/C++/main.cpp" "/mnt/e/Drive/Projects/ProgrammingPractice/LeetCode/TwoSum/C++/cmake-build-debug/CMakeFiles/TwoSum.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- LeetCode/TwoSum/C++/main.cpp
+++ LeetCode/TwoSum/C++/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    for(int i=0;i<nums.size();i++) {
+        for(int j=i+1;j<nums.size();j++) {
+            if(nums.at(j) == target - nums.at(i)) {
+                std::vector<int> indices;
+                indices.push_back(i);
+                indices.push_back(j);
+                return indices;
+            }
+        }
+    }
+
+    std::vector<int> error = { INT32_MAX, INT32_MAX };
+    return error;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> input = { 2, 7, 11, 15 };
+    std::vector<int> result = twoSum(input, 9);
+    std::cout << result[0] << " " << result[1] << std::endl;
     return 0;
 }
