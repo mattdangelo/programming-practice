a036b4b316859cc0492e6a476e8e1d06f67fc087
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-10-27 12:06:05 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- LeetCode/problems/two_sum/solution.cpp
+++ LeetCode/problems/two_sum/solution.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    for(int i=0;i<nums.size();i++) {
-        for(int j=i+1;j<nums.size();j++) {
-            if(nums.at(j) == target - nums.at(i)) {
-                std::vector<int> indices;
-                indices.push_back(i);
-                indices.push_back(j);
-                return indices;
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> val_to_index;
+
+        for(int i=0;i<nums.size();i++) {
+            int to_find = target - nums[i];
+
+            if(val_to_index.count(to_find)) {
+                return { val_to_index[to_find], i };
+            }
+            else {
+                val_to_index[nums[i]] = i ;
             }
         }
-    }
 
-    std::vector<int> error = { INT32_MAX, INT32_MAX };
-    return error;
-}
+        std::vector<int> error = { INT32_MAX, INT32_MAX };
+        return error;
+    }
 };
