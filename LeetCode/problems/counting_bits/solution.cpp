83fdf87a60f759f300a4cd918e45d656c815b053
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-10-30 09:44:32 +0000

    Sync LeetCode submission - Counting Bits (cpp)

--- LeetCode/problems/counting_bits/solution.cpp
+++ LeetCode/problems/counting_bits/solution.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        auto result = std::vector<int>();
-        result.reserve(n + 1);
-        result.push_back(0);
-        for (int i = 1; i < n+1; i++) {
-            int query = i;
-            int count = 0;
-            while (query) {
-                if (query & 1) {
-                    count++;
-                }
-                query = query >> 1;
+        std::vector<int> result(n + 1, 0);
+        for(unsigned int i=1;i<=n;i++) {
+            int res = result[i >> 1];
+            if(i&1) {
+                res++;
             }
-            result.push_back(count);
+            result[i] = res;
         }
+
         return result;
     }
 };
