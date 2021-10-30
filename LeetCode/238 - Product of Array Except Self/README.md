9da3cd1caad35dfa7a6a3880011a72b1a79cfd0d
matt <matthew.dangelo6@gmail.com>
2021-10-30 19:43:10 +1030

    Add Product of Array Except Self

---
+++ LeetCode/238 - Product of Array Except Self/README.md
@@ -0,0 +1,10 @@
+# Product of Array Except Self
+
+### https://leetcode.com/problems/product-of-array-except-self/
+
+### Notes:
+
+* Keep track of the left and right partial products in separate arrays
+* Go through and multiply elements left and right of index i
+* Watch out for bounds
+* TODO: 1 pass?
---
+++ LeetCode/238 - Product of Array Except Self/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> productExceptSelf(std::vector<int>& nums) {
+    std::vector<int> left = { nums[0] };
+    std::vector<int> right = { nums[nums.size() - 1] };
+
+    for(int i = 1; i < nums.size();i++) {
+        left.push_back(left[i - 1] * nums[i]);
+        right.push_back(right[i - 1] * nums[nums.size() - i - 1]);
+    }
+
+    std::vector<int> result;
+    for(int i=0;i<nums.size();i++) {
+        int l = i == 0 ? 1 : left[i - 1];
+        int r = i == nums.size() - 1 ? 1 : right[nums.size() - i - 2];
+        result.push_back(l * r);
+    }
+
+    return result;
+}
+
+int main() {
+    std::vector<int> input = { -1, 1, 0, -3, 3 };
+    std::vector<int> result = productExceptSelf(input);
+
+    for(int i : result) {
+        std::cout << i << " " << std::endl;
+    }
+
+    return 0;
+}
