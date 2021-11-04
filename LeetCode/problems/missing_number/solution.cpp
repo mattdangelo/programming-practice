32c91db0496e215d4c641812d437d7b1eb0b7b45
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-11-04 10:50:57 +0000

    Sync LeetCode submission - Missing Number (cpp)

--- LeetCode/problems/missing_number/solution.cpp
+++ LeetCode/problems/missing_number/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-    int sum = 0;
-    int size = nums.size();
-    for (int i = 0; i < size + 1;i++) {
-        sum += i;
-        if (i != size) {
-            sum -= nums.at(i);
+        int sum = 0;
+        int actual = 0;
+        for(int i=0;i<nums.size();i++) {
+            sum += nums[i];
+            actual += (i + 1);
         }
-    }
 
-    return sum;
+        return actual - sum;
     }
 };
