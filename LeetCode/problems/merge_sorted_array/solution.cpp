3fa4aa08efff3bb005c0333b5204b96349e85c4e
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-11-16 11:28:17 +0000

    Sync LeetCode submission - Merge Sorted Array (cpp)

--- LeetCode/problems/merge_sorted_array/solution.cpp
+++ LeetCode/problems/merge_sorted_array/solution.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        if(n == 0) {
-          return;
-        }
-        else {
-            for(int i=0;i<nums2.size();i++) {
-                nums1[m + i] = nums2[i];
+        std::queue<int> extra;
+        
+        int end_1 = m - 1;
+        int end_2 = n - 1;
+        int end_l = n + m - 1;
+        
+        while(end_1 >= 0 && end_2 >= 0) {
+            if(nums1[end_1] > nums2[end_2]) {
+                nums1[end_l] = nums1[end_1];
+                end_l--;
+                end_1--;
+            }
+            else {
+                nums1[end_l] = nums2[end_2];
+                end_l--;
+                end_2--;
             }
         }
-
-        std::sort(nums1.begin(), nums1.end());
+        
+        while(end_2 >= 0) {
+            nums1[end_l] = nums2[end_2];
+            end_l--;
+            end_2--;
+        }
     }
 };
