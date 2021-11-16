d7298d09076cc9b76179f3db48757525703bb041
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-11-16 00:55:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- LeetCode/0070-climbing-stairs/solution.cpp
+++ LeetCode/0070-climbing-stairs/solution.cpp
@@ -7,13 +7,13 @@ public:
         }
 
         if(memo.find(n - 1) == memo.end()) {
-            memo[n-1] = climbStairs(n - 1);
+            memo[n - 1] = climbStairs(n - 1);
         }
-
+        
         if(memo.find(n - 2) == memo.end()) {
-            memo[n-2] = climbStairs(n - 2);
+            memo[n - 2] = climbStairs(n - 2);
         }
-
-        return memo[n-1] + memo[n-2];
+        
+        return memo[n - 1] + memo[n - 2];
     }
 };
