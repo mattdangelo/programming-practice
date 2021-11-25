d416e1913fe2f92ad66d0e72c8430596497d3bd2
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-11-25 02:02:12 +0000

    Sync LeetCode submission Runtime - 186 ms (5.28%), Memory - 67.7 MB (99.88%)

--- LeetCode/0053-maximum-subarray/solution.cpp
+++ LeetCode/0053-maximum-subarray/solution.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int current_max = INT32_MIN;
-        int best_max = INT32_MIN;
+        int best = INT32_MIN;
         
-        for(int i : nums) {
-            if(current_max == INT32_MIN) {
-                current_max = i;
+        int ptr = 0;
+        int local_best = 0;
+        
+        while(ptr < nums.size()) {
+            local_best += nums[ptr];
+
+            if(local_best > best) {
+                best = local_best;
             }
-            else {
-                current_max += i;
-            }
-            
-            if(current_max > best_max) {
-                best_max = current_max;
-            }
-            
-            if(current_max < 0) {
-                current_max = 0;
+
+            if(local_best < 0) {
+                local_best = 0;
             }
             
+            ptr++;
         }
         
-        return best_max;
+        return best;
     }
 };
