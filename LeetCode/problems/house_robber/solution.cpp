5f77ae04fc1bca124bc7849d653ce2c33b68abdf
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-11-26 10:33:47 +0000

    Sync LeetCode submission - House Robber (cpp)

--- LeetCode/problems/house_robber/solution.cpp
+++ LeetCode/problems/house_robber/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
-    std::unordered_map<int, int> memo;
+    unordered_map<int, int> memo;
 public:
-    int rob(vector<int>& nums, int index) {
-        if(index < 0) {
+    int recRob(vector<int>& nums, int index) {
+        if(index >= nums.size()) {
             return 0;
         }
         
-        if(memo.find(index-1) == memo.end()) {
-            memo[index-1] = rob(nums, index - 1);
-        }
-                                
-        if(memo.find(index-2) == memo.end()) {
-            memo[index-2] = rob(nums, index - 2);
+        if(memo.count(index + 1) == 0) {
+            memo[index + 1] = recRob(nums, index + 1);
         }
         
-        return max(memo[index-1], memo[index-2] + nums[index]);
+        if(memo.count(index + 2) == 0) {
+            memo[index + 2] = recRob(nums, index + 2);
+        }
+        
+        return max(memo[index + 1], memo[index + 2] + nums[index]);
     }
     
     int rob(vector<int>& nums) {
-        return rob(nums, nums.size() - 1);
+        return recRob(nums, 0);
     }
 };
