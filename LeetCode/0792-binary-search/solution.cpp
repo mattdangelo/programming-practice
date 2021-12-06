d51eb3f34cc4777f762d341a214d0b88f0c617d3
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-06 02:43:26 +0000

    Sync LeetCode submission Runtime - 28 ms (49.80%), Memory - 27.6 MB (99.66%)

--- LeetCode/0792-binary-search/solution.cpp
+++ LeetCode/0792-binary-search/solution.cpp
@@ -4,23 +4,19 @@ public:
         int left = 0;
         int right = nums.size() - 1;
         
-        while(left < right) {
-            int mid = (left + right) / 2;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
             if(nums[mid] == target) {
                 return mid;
             }
             else if(nums[mid] < target) {
                 left = mid + 1;
             }
-            else if(nums[mid] > target) {
+            else {
                 right = mid - 1;
             }
         }
         
-        if(nums[left] != target) {
-            return -1;
-        }
-        
-        return left;
+        return -1;
     }
 };
