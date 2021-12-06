381074a8a9bee1e70be0e778157a07db80e329d9
matt <matthew.dangelo6@gmail.com>
2021-12-06 13:14:53 +1030

    Add Binary Search

---
+++ LeetCode/704 - Binary Search/README.md
@@ -0,0 +1,8 @@
+# Binary Search
+
+### https://leetcode.com/problems/binary-search/
+
+### Notes:
+
+* Left and right pointers
+* Set to midpoint depending on if nums[mid] > or < target value
---
+++ LeetCode/704 - Binary Search/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int search(std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    std::cout << search( {-1, 0, 3, 5, 9, 12}, 9) << std::endl;
+    return 0;
+}
