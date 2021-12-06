60fc0f4c93c2b185bb52f6544910ca6efc44cef4
matt <matthew.dangelo6@gmail.com>
2021-12-06 22:22:19 +1030

    Add Search Insert Position

---
+++ LeetCode/35 - Search Insert Position/README.md
@@ -0,0 +1,7 @@
+# Search Insert Position
+
+### https://leetcode.com/problems/search-insert-position/
+
+### Notes:
+
+* Basic binary search, return value[left] when you're done
---
+++ LeetCode/35 - Search Insert Position/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+int searchInsert(std::vector<int>& nums, int target) {
+	int left = 0;
+	int right = nums.size() - 1;
+
+	while (left <= right) {
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		}
+		else if (nums[mid] < target) {
+			left = mid + 1;
+		}
+		else {
+			right = mid - 1;
+		}
+	}
+
+	return left;
+}
+
+int main() {
+	std::cout << searchInsert({ 1, 3, 5, 6 }, 5);
+	return 0;
+}
