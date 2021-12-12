8e776385cfdd0c9cc14b4ef70ec1457fe9523643
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-12 10:16:20 +0000

    Sync LeetCode submission Runtime - 4 ms (59.62%), Memory - 10.1 MB (100.00%)

--- LeetCode/0350-intersection-of-two-arrays-ii/solution.cpp
+++ LeetCode/0350-intersection-of-two-arrays-ii/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        std::vector<int> res;
-        
         std::sort(nums1.begin(), nums1.end());
         std::sort(nums2.begin(), nums2.end());
 
-        int n1_ptr = 0;
-        int n2_ptr = 0;
-        
-        while(n1_ptr < nums1.size() && n2_ptr < nums2.size()) {
-            if(nums1[n1_ptr] < nums2[n2_ptr]) {
-                n1_ptr++;
+        int l1_ptr = 0;
+        int l2_ptr = 0;
+
+        std::vector<int> result;
+
+        while (l1_ptr != nums1.size() && l2_ptr != nums2.size()) {
+            if (nums1[l1_ptr] == nums2[l2_ptr]) {
+                result.push_back(nums1[l1_ptr]);
+                l1_ptr++;
+                l2_ptr++;
             }
-            else if(nums1[n1_ptr] > nums2[n2_ptr]) {
-                n2_ptr++;
+            else if (nums1[l1_ptr] < nums2[l2_ptr]) {
+                l1_ptr++;
             }
             else {
-                res.push_back(nums1[n1_ptr]);
-                n1_ptr++;
-                n2_ptr++;
-            }   
+                l2_ptr++;
+            }
         }
-        
-        return res;
+
+        return result;
     }
 };
