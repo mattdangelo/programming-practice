c36f5399bf4d08bad794be6e246fd398e9895ff1
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-12 09:07:02 +0000

    Sync LeetCode submission Runtime - 4 ms (89.47%), Memory - 9.6 MB (100.00%)

--- LeetCode/0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ LeetCode/0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0;
-        int r = numbers.size() - 1;
-        
-        while(l < r) {
-            int sum = numbers[l] + numbers[r];
-            if(sum == target) {
-                std::vector<int> res = {l + 1, r + 1};
-                return res;
+        int l_ptr = 0;
+        int r_ptr = numbers.size() - 1;
+        int sum;
+
+        while (l_ptr < r_ptr) {
+            sum = numbers[l_ptr] + numbers[r_ptr];
+            if (sum == target) {
+                return {l_ptr + 1, r_ptr + 1};
             }
-            else if(sum < target) {
-                l++;
+            else if (sum < target) {
+                l_ptr++;
             }
             else {
-                r--;
+                r_ptr--;
             }
         }
-        
-        return std::vector<int>();
+
+        return { -1, -1 };
     }
 };
