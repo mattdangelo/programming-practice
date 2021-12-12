811d3ee20998eaa459e803a6b5c2052783813d3d
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-12 04:13:20 +0000

    Sync LeetCode submission Runtime - 47 ms (6.21%), Memory - 24.9 MB (100.00%)

--- LeetCode/0189-rotate-array/solution.cpp
+++ LeetCode/0189-rotate-array/solution.cpp
@@ -1,14 +1,23 @@
 class Solution {
+    void swap(std::vector<int>& nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 public:
     void rotate(vector<int>& nums, int k) {
-        std::vector<int> cpy(nums.size());
-        
-        for(int i=0;i<nums.size();i++) {
-            cpy[i] = nums[i];
+        k = k % nums.size();
+
+        for (int i = 0; i < nums.size() / 2; i++) {
+            swap(nums, i, nums.size() - 1 - i);
         }
 
-        for(int i=0;i<nums.size();i++) {
-            nums[(i + k) % nums.size()] = cpy[i];
+        for (int i = 0; i < k / 2; i++) {
+            swap(nums, i, k - i - 1);
+        }
+
+        for (int i = 0; i < (nums.size() - k) / 2; i++) {
+            swap(nums, i + k, nums.size() - 1 - i);
         }
     }
 };
