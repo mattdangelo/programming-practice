5b22fc3588c51131c8edf9abb3c3c830af32a91c
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-14 07:22:46 +0000

    Sync LeetCode submission - Jump Game (cpp)

--- LeetCode/problems/jump_game/solution.cpp
+++ LeetCode/problems/jump_game/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int best = 0;
-        for(int i = 0; i <= best; i++) {
+        for(int i=0;i<=best;i++) {
             best = max(best, i + nums[i]);
             if(best >= nums.size() - 1) {
                 return true;
