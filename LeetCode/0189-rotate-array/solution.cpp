d35890cd9671cdbeeba3e018e58c1c03a5488953
Matt D'Angelo <Astradus@users.noreply.github.com>
2021-12-18 04:38:04 +0000

    Sync LeetCode submission Runtime - 47 ms (6.21%), Memory - 25.1 MB (100.00%)

--- LeetCode/0189-rotate-array/solution.cpp
+++ LeetCode/0189-rotate-array/solution.cpp
@@ -6,16 +6,16 @@ class Solution {
     }
 public:
     void rotate(vector<int>& nums, int k) {
-        k = k % nums.size();
-
+        k = k%nums.size();
         for (int i = 0; i < nums.size() / 2; i++) {
             swap(nums, i, nums.size() - 1 - i);
         }
-
+        
         for (int i = 0; i < k / 2; i++) {
             swap(nums, i, k - i - 1);
         }
-
+        
+        
         for (int i = 0; i < (nums.size() - k) / 2; i++) {
             swap(nums, i + k, nums.size() - 1 - i);
         }
