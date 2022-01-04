409130c434f8f646d3bca58d78ce0fb64546a53e
Matt D'Angelo <Astradus@users.noreply.github.com>
2022-01-04 00:32:21 +0000

    Sync LeetCode submission Runtime - 2 ms (48.53%), Memory - 5.9 MB (100.00%)

--- LeetCode/1054-complement-of-base-10-integer/solution.cpp
+++ LeetCode/1054-complement-of-base-10-integer/solution.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int bitwiseComplement(int n) {
         if(n == 0) {
-            return 1;    
+            return 1;
+        }
+        int ret = 0;
+        int shift = 0;
+        while(n) {
+            ret |= ((!(n & 1)) << shift);
+            n = n >> 1;
+            shift++;
         }
         
-        long pow = 1;
-        int num_c = n;
-        while(num_c) {
-            num_c = num_c >> 1;
-            pow = pow << 1;   
-        }
-        return n ^ (pow - 1);
+        return ret;
     }
 };
