3bafa9ebc5fdfce9cc3822fa99704a24ba753529
Matt D'Angelo <Astradus@users.noreply.github.com>
2022-01-06 10:54:24 +0000

    Sync LeetCode submission Runtime - 63 ms (11.25%), Memory - 10.7 MB (75.69%)

--- LeetCode/0003-longest-substring-without-repeating-characters/solution.cpp
+++ LeetCode/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        std::unordered_set<char> found;
+        int left = 0;
+        int right = 0;
+        
+        unordered_set<char> found;
+
         int best = 0;
-        
-        int l = 0;
-        int r = 0;
-        
-        while(r < s.length()) {
-            if(found.count(s[r])) {
-                // Have seen this value
-                while(s[l] != s[r]) {
-                    found.erase(s[l]);
-                    l++;
+        while(right < s.length()) {
+            if(found.count(s[right]) > 0) {
+                while(s[left] != s[right]) {
+                    found.erase(s[left]);
+                    left++;
                 }
-                found.erase(s[l]);
-                l++;
+                
+                found.erase(s[left]);
+                left++;
             }
             else {
-                found.insert(s[r]);
-                r++;
-                if((r - l + 1) > best) {
-                    best = (r - l);
-                }
+                found.insert(s[right]);
+                right++;
+                best = max(best, right - left);
             }
         }
         
