73278b4d5738360e9e5da84d5406c543b052e4e5
Matt D'Angelo <Astradus@users.noreply.github.com>
2022-01-25 05:47:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 5.8 MB (100.00%)

---
+++ LeetCode/0520-detect-capital/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count = 0;
+        for(char c : word) {
+            if(isupper(c)) {
+                count++;
+            }
+        }
+        
+        return (count == 0) || 
+               (count == 1 && isupper(word[0])) || 
+               count == word.length();
+    }
+};
