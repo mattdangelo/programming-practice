38ce280f1f51b06778c78ad23440d02138f9a838
Matt D'Angelo <Astradus@users.noreply.github.com>
2022-07-04 02:23:10 +0000

    Sync LeetCode submission - Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (cpp)

--- LeetCode/problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
+++ LeetCode/problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
@@ -1,41 +1,29 @@
 class Solution {
+    long getMaxDiff(int boundry, vector<int>& cuts) {
+        int max;
+
+        if(cuts.size() == 0) {
+            // Special case for 0 length array
+            max = boundry;
+        }
+        else {
+            std::sort(cuts.begin(), cuts.end());
+            int prev = 0;
+            max = 1;
+            int diff;
+
+            for(int i : cuts) {
+                diff = i - prev;
+                max = std::max(max, diff);
+                prev = i;
+            }
+            max = std::max(max, boundry - prev);   
+        }
+
+        return max;
+    }
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
-        int max_horiz = 1;
-        int max_vert = 1;
-        
-        std::sort(horizontalCuts.begin(), horizontalCuts.end());
-        std::sort(verticalCuts.begin(), verticalCuts.end());
-        
-        int prev = 0;
-
-        // Special case for either array being 0 length
-        if(horizontalCuts.size() == 0) {
-            max_horiz = h;
-        }
-        else {
-            for(int i : horizontalCuts) {
-                int diff = i - prev;
-                max_horiz = std::max(max_horiz, diff);
-                prev = i;
-            }
-            max_horiz = std::max(max_horiz, h - prev);   
-        }
-        
-        if(verticalCuts.size() == 0) {
-            max_vert = w;
-        }
-        else {
-            prev = 0;
-            for(int i : verticalCuts) {
-                int diff = i - prev;
-                max_vert = std::max(max_vert, diff);
-                prev = i;
-            }
-            max_vert = std::max(max_vert, w - prev);   
-        }
-        
-        long long result = (long long)max_horiz * (long long)max_vert;
-        return (result % 1000000007);
+        return (getMaxDiff(w, verticalCuts) * getMaxDiff(h, horizontalCuts)) % 1000000007;
     }
 };
