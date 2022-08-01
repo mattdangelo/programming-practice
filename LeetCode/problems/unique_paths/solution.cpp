6cf9b9c1f23b65cff1de1641b77b2c263997a0f8
Matt D'Angelo <Astradus@users.noreply.github.com>
2022-08-01 12:32:30 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- LeetCode/problems/unique_paths/solution.cpp
+++ LeetCode/problems/unique_paths/solution.cpp
@@ -1,30 +1,29 @@
 class Solution {
-    std::unordered_map<uint64_t, int> memo;
+    int memo[101][101];
 public:
-    int uniquePaths(int m, int n) {
-        if(m == 1) {
-            return 1;
-        }
-        else if(n == 1) {
+    int recUniquePaths(int m, int n) {
+        if((m <= 1) || (n <= 1)) {
             return 1;
         }
         
-        uint64_t key_a = min(n - 1, m);
-        key_a = key_a << 32;
-        key_a = key_a | max(m, n - 1);
+        if(memo[n - 1][m] == -1) {
+            memo[n - 1][m] = recUniquePaths(n - 1, m);
+        }
+        
+        if(memo[n][m - 1] == -1) {
+            memo[n][m - 1] = recUniquePaths(n, m - 1);
+        }
+        
+        return memo[n - 1][m] + memo[n][m - 1];
+    }
 
-        uint64_t key_b = min(n, m - 1);
-        key_b = key_b << 32;
-        key_b = key_b | max(m - 1, n);
-        
-        if(memo.find(key_a) == memo.end()) {
-            memo[key_a] = uniquePaths(m, n-1);
+    int uniquePaths(int m, int n) {
+        for(int i=0;i<101;i++) {
+            for (int j=0;j<101;j++) {
+                memo[i][j] = -1;
+            }
         }
         
-        if(memo.find(key_b) == memo.end()) {
-            memo[key_b] = uniquePaths(m - 1, n);
-        }
-        
-        return memo[key_a] + memo[key_b];
+        return recUniquePaths(m, n);
     }
 };
