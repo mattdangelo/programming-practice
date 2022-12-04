7070e2766da119ed48d02339932c674f107086bd
Astradus <matthew.dangelo6@gmail.com>
2022-12-04 15:50:59 +1030

    Add Day 4: Camp Cleanup

---
+++ AdventOfCode/2022/4 - Camp Cleanup/Part 2/main.rb
@@ -0,0 +1,6 @@
+def fully_contains(string)
+  r1, r2 = string.split(',').map { |r| r.split('-').map(&:to_i) }
+  r2[0] <= r1[1] && r1[0] <= r2[1]
+end
+
+p File.readlines("4 - Camp Cleanup/values.txt").count { |line| fully_contains(line) }
