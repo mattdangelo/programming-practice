1dd7dfeacdb8a0b92a29f5bb7e53599467c11d91
Astradus <matthew.dangelo6@gmail.com>
2022-12-07 21:56:16 +1030

    Add Day 6: Tuning Trouble

---
+++ AdventOfCode/2022/6 - Tuning Trouble/Part 2/main.rb
@@ -0,0 +1,31 @@
+WINDOW_LENGTH = 14
+
+data = File.read("6 - Tuning Trouble/values.txt")
+
+window = {}
+
+def add_to_window(c, in_window)
+  in_window.key?(c) ? in_window[c] += 1 : in_window[c] = 1
+end
+
+def remove_from_window(c, in_window)
+  in_window[c] == 1 ? in_window.delete(c) : in_window[c] -= 1
+end
+
+left = 0
+right = 0
+
+# Initial characters
+while right - left < WINDOW_LENGTH do
+  add_to_window(data[right], window)
+  right += 1
+end
+
+while window.size < WINDOW_LENGTH do
+  remove_from_window(data[left], window)
+  add_to_window(data[right], window)
+  left += 1
+  right += 1
+end
+
+p right
