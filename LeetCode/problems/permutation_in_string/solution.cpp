10f6d0f7e5439b42ca7f3d117b0d21ae2685ceaa
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-02-04 07:10:24 +0000

    Sync LeetCode submission - Permutation in String (cpp)

--- LeetCode/problems/permutation_in_string/solution.cpp
+++ LeetCode/problems/permutation_in_string/solution.cpp
@@ -5,41 +5,15 @@ public:
             false;
         }
 
-        std::unordered_map<char, int> s1_chars;
-        for (char c : s1) {
-            if (s1_chars.count(c) == 0) {
-                s1_chars[c] = 1;
-            }
-            else {
-                s1_chars[c]++;
-            }
-        }
-
         // Sliding window
         int left = 0;
         int right = s1.length();
+        sort(s1.begin(), s1.end());
 
         while (right <= s2.length()) {
-            auto s1_chars_cpy = s1_chars;
-            bool skip_check = false;
-            for (int i = left; i < right; i++) {
-                char to_rem = s2[i];
-                if (s1_chars_cpy.count(to_rem) == 0) {
-                    skip_check = true;
-                    break;
-                }
-                else if (s1_chars_cpy.count(to_rem)) {
-                    // Count is at least 1
-                    if (s1_chars_cpy[to_rem] == 1) {
-                        s1_chars_cpy.erase(to_rem);
-                    }
-                    else {
-                        s1_chars_cpy[to_rem]--;
-                    }
-                }
-            }
-
-            if (!skip_check && s1_chars_cpy.empty()) {
+            string s2_substr = s2.substr(left, s1.length());
+            sort(s2_substr.begin(), s2_substr.end());
+            if(s2_substr == s1) {
                 return true;
             }
 
