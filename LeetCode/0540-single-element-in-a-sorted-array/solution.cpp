200040cc7395a41b5bdd9992f70fd4cbcbdd73d7
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-02-27 13:08:41 +0000

    Sync LeetCode submission Runtime - 29 ms (5.11%), Memory - 22.3 MB (100.00%)

--- LeetCode/0540-single-element-in-a-sorted-array/solution.cpp
+++ LeetCode/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,20 +1,54 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int low = 0, high = nums.size() - 2; 
-        
-        while(low <= high){
+        // Trivial case, size == 1
+        if(nums.size() == 1) {
+            return nums[0];
+        }
+
+        // Perform binary search
+        int left = 0;
+        int right = nums.size();
+        int mid;
+
+        while(true) {
+            mid = left + (right - left) / 2;
             
-            int mid = low + (high - low) / 2;
-            
-            if(nums[mid] == nums[mid^1]) {
-                low = mid + 1;
+            if(isSingleElement(nums, mid)) {
+                return nums[mid];
+            }
+
+            if(singleElementHasAlreadyOccurred(nums, mid)) {
+                right = mid - 1;
             }
             else {
-                high = mid - 1;
-            };
+                left = mid + 1;
+            }
         }
-        
-        return nums[low];
+    }
+private:
+    bool singleElementHasAlreadyOccurred(vector<int>& nums, int index) {
+        if(index % 2 == 0) {
+            return nums[index + 1] != nums[index];
+        }
+        else {
+            if(index + 1 == nums.size()) {
+                return true;
+            }
+
+            return nums[index + 1] == nums[index];
+        }
+    }
+
+    bool isSingleElement(vector<int>& nums, int index) {
+        if(index - 1 < 0) {
+            return nums[index] != nums[index + 1];
+        }
+
+        if(index + 1 == nums.size()) {
+            return nums[index] != nums[index - 1];
+        }
+
+        return (nums[index] != nums[index - 1]) && (nums[index] != nums[index + 1]);
     }
 };
