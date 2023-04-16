3743ee3e9e807eaaacf82bd51b20fa503b9201b7
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-04-16 11:05:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- LeetCode/0020-valid-parentheses/solution.cpp
+++ LeetCode/0020-valid-parentheses/solution.cpp
@@ -1,32 +1,24 @@
 class Solution {
+    const std::unordered_map<char, char> opp = {{'(', ')'}, {'[', ']'}, {'{', '}'}};
+    std::stack<char> stack;
 public:
     bool isValid(string s) {
-        std::stack<int> p_stack;
-        std::unordered_map<char, char> p_pairs = {
-            {'(', ')'},
-            {'[', ']'},
-            {'{', '}'}
-        };
-        
         for(char c : s) {
-            if(p_pairs.count(c)) {
-                // It's an opening parenthesis
-                p_stack.push(c);
+            if(opp.count(c)) {
+                // Opening Brace
+                stack.push(c);
             }
             else {
-                if(p_stack.size() == 0) {
+                // Closing Brace
+                if(stack.empty() || c != opp.at(stack.top())) {
                     return false;
-                }
-                
-                if(p_pairs.count(p_stack.top()) && p_pairs[p_stack.top()] == c) {
-                    p_stack.pop();
                 }
                 else {
-                    return false;
+                    stack.pop();
                 }
             }
         }
-        
-        return p_stack.size() == 0;
+
+        return stack.empty();
     }
 };
