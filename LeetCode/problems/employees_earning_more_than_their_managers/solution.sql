175f4766c2b11e4d78845074a4200e38b748a542
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-05-27 05:07:56 +0000

    Sync LeetCode submission - Employees Earning More Than Their Managers (mysql)

---
+++ LeetCode/problems/employees_earning_more_than_their_managers/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT employee.name AS Employee
+FROM Employee employee
+LEFT JOIN Employee manager ON employee.managerId = manager.id
+WHERE employee.salary > manager.salary
