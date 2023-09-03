36887aec72325712ecd28ce00893f5700745b9e2
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-09-03 01:54:48 +0000

    Sync LeetCode submission - Unique Paths (cpp)

--- LeetCode/problems/unique_paths/solution.cpp
+++ LeetCode/problems/unique_paths/solution.cpp
@@ -1,29 +1,32 @@
 class Solution {
-    int memo[101][101];
 public:
-    int recUniquePaths(int m, int n) {
-        if((m <= 1) || (n <= 1)) {
+    int recUniquePaths(int m, int n, vector<vector<int>>& cache) {
+        if(cache[m][n] != -1) {
+            return cache[m][n];
+        }
+   
+        // Base case
+        if(m == 1 && n == 0 || m == 0 && n == 1) {
             return 1;
         }
-        
-        if(memo[n - 1][m] == -1) {
-            memo[n - 1][m] = recUniquePaths(n - 1, m);
+        else if(m == 1) {
+            cache[m][n - 1] = recUniquePaths(m, n - 1, cache);
+            return cache[m][n - 1];
         }
-        
-        if(memo[n][m - 1] == -1) {
-            memo[n][m - 1] = recUniquePaths(n, m - 1);
+        else if(n == 1) {
+            cache[m - 1][n] = recUniquePaths(m - 1, n, cache);
+            return cache[m - 1][n];
         }
-        
-        return memo[n - 1][m] + memo[n][m - 1];
+
+        // Go down OR right
+        cache[m - 1][n] = recUniquePaths(m - 1, n, cache);
+        cache[m][n - 1] = recUniquePaths(m, n - 1, cache);
+        return cache[m - 1][n] + cache[m][n - 1];
     }
 
     int uniquePaths(int m, int n) {
-        for(int i=0;i<101;i++) {
-            for (int j=0;j<101;j++) {
-                memo[i][j] = -1;
-            }
-        }
-        
-        return recUniquePaths(m, n);
+        std::vector<std::vector<int>> cache(m + 1, std::vector<int> (n + 1, -1));
+        int count = recUniquePaths(m, n, cache);
+        return count;
     }
 };
