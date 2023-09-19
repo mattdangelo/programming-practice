e086fea7dfd1b241001deab8f101e22ed7554cab
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-09-19 09:25:20 +0000

    Sync LeetCode submission Runtime - 4 ms (35.35%), Memory - 7.1 MB (100.00%)

--- LeetCode/0118-pascals-triangle/solution.cpp
+++ LeetCode/0118-pascals-triangle/solution.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> result;
-        result.reserve(numRows);
-        result.push_back({1});
-        
-        if(numRows == 1) {
-            return result;
+        vector<vector<int>> res(numRows);
+        res[0] = {1};
+        for(int i=1;i<numRows;i++) {
+            vector<int> row;
+            row.reserve(i + 1);
+            for(int j=0;j<i+1;j++) {
+                int left, right;
+                if(j - 1 < 0) {
+                    left = 0;
+                }
+                else {
+                    left = res[i - 1][j - 1];
+                }
+
+                if(j >= res[i - 1].size()) {
+                    right = 0;
+                }
+                else {
+                    right = res[i - 1][j];
+                }
+                row.push_back(left + right);
+            }
+            res[i] = row;
         }
 
-        for(int i=1;i<numRows;i++) {
-            std::vector<int> n;
-            n.reserve(i + 1);
-            n.push_back(1);
-            for(int j=1;j<i;j++) {
-                n.push_back(result.at(i-1)[j-1] + result.at(i-1)[j]);
-            }
-            n.push_back(1);
-            result.push_back(n);
-        }
-        return result;
+        return res;
     }
 };
