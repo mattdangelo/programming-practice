778ea89310b2a3af8419d85395a15799b1418e9f
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-09-25 09:58:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.1 MB (100.00%)

--- LeetCode/0389-find-the-difference/solution.cpp
+++ LeetCode/0389-find-the-difference/solution.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        std::unordered_map<char, int> s_m;
-        
-        for(int i=0;i<s.length();i++) {
-            if(s_m.count(s[i])) {
-                if(s_m[s[i]] == -1) {
-                    s_m.erase(s[i]);
-                }
-                else {
-                   s_m[s[i]]++; 
-                }
+        std::unordered_map<char, int> count;
+        for(char c : t) {
+            if(count.count(c)) {
+                count[c]++;
             }
             else {
-                s_m[s[i]] = 1;
-            }
-            
-            if(s_m.count(t[i])) {
-                if(s_m[t[i]] == 1) {
-                    s_m.erase(t[i]);
-                }
-                else {
-                    s_m[t[i]]--;
-                }
-            }
-            else {
-                s_m[t[i]] = -1;
+                count[c] = 1;
             }
         }
-        
-        if(s_m.count(t[t.length() - 1])) {
-            if(s_m[t[t.length() - 1]] == 1) {
-                s_m.erase(t[t.length() - 1]);
-            }
-            else {
-                s_m[t[t.length() - 1]]--;
+
+        for(char c : s) {
+            count[c]--;
+            if(count[c] == 0) {
+                count.erase(c);
             }
         }
-        else {
-            s_m[t[t.length() - 1]] = -1;
+
+        for(std::pair<char, int> p : count) {
+            return p.first;
         }
-        
-        return s_m.begin()->first;
+
+        return ' ';
     }
 };
