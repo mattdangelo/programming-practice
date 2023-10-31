a8254ff436220f1508720337be433b9005f88cef
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-10-31 01:10:31 +0000

    Sync LeetCode submission Runtime - 82 ms (35.65%), Memory - 76.3 MB (99.80%)

---
+++ LeetCode/2519-find-the-original-array-of-prefix-xor/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> res(pref.size());
+        res[0] = pref[0];
+        for(int i=1;i<pref.size();i++) {
+            res[i] = pref[i - 1] ^ pref[i];
+        }
+
+        return res;
+    }
+};
