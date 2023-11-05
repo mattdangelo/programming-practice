3a3600076441c207554ce750fdcab5025c162786
Matt D'Angelo <Astradus@users.noreply.github.com>
2023-11-05 07:23:24 +0000

    Sync LeetCode submission Runtime - 1553 ms (94.58%), Memory - 0B (100.00%)

--- LeetCode/0577-employee-bonus/solution.sql
+++ LeetCode/0577-employee-bonus/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 
 SELECT Employee.name, Bonus.bonus 
-FROM Employee LEFT JOIN Bonus ON Employee.empId = Bonus.empId
-WHERE Bonus.bonus IS NULL OR Bonus.bonus < 1000
+FROM Employee LEFT JOIN Bonus bonus ON Employee.empId = Bonus.empId
+WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL
