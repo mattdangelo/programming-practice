2253584bed2375a0ed5ddfc8228a2e3fdaa54a6f
Astradus <matthew.dangelo6@gmail.com>
2023-12-03 13:10:31 +1030

    Add Day 1: Trebuchet?!

---
+++ AdventOfCode/2023/1 - Trebuchet!/Part 2/main.rb
@@ -0,0 +1,14 @@
+sum = 0
+word_numbers = { "zero" => 1, "one" => 1, "two" => 2, "three" => 3, "four" => 4, "five" => 5, "six" => 6, "seven" => 7, "eight" => 8, "nine" => 9 }
+pattern =/(?=(\d|#{word_numbers.keys.join('|')}))/
+
+File.readlines("1 - Trebuchet!/values.txt").each do |line|
+  matches = line.scan(pattern)
+
+  first_match = word_numbers[matches.first[0]] || matches.first[0]
+  last_match = word_numbers[matches.last[0]] || matches.last[0]
+
+  sum += "#{first_match}#{last_match}".to_i
+end
+
+p sum
