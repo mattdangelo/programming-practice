cf2639d13f00224fb3ea6261e07bd964a5bcd0a4
Matt D'Angelo <Astradus@users.noreply.github.com>
2024-01-18 07:21:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- LeetCode/0070-climbing-stairs/solution.cpp
+++ LeetCode/0070-climbing-stairs/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
-    unordered_map<int, int> memo;
+    // Cache
+    int cache[46] = {};
 public:
-    int climbStairs(int n) {
-        if(n == 0 || n == 1) {
-            return 1;
+    Solution() {
+        // Base cases (0, 1 and 2 steps)
+        for(int i=0;i<3;i++) {
+            cache[i] = i;
         }
-        
-        if(memo.count(n - 1) == 0) {
-            memo[n - 1] = climbStairs(n - 1);
+    }
+
+    int climbStairs(int n) { 
+        int ctr = 3;
+        while(ctr <= n) {
+            cache[ctr] = cache[ctr - 1] + cache[ctr - 2];
+            ctr++;
         }
 
-        if(memo.count(n - 2) == 0) {
-            memo[n - 2] = climbStairs(n - 2);
-        }
-        
-        return memo[n - 1] + memo[n - 2];
+        return cache[n];
     }
 };
