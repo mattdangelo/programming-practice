3571ffe871d9a3a5f8117e600a2089e76346aada
Matt D'Angelo <Astradus@users.noreply.github.com>
2024-02-19 08:10:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.2 MB (35.87%)

--- LeetCode/0231-power-of-two/solution.cpp
+++ LeetCode/0231-power-of-two/solution.cpp
@@ -1,9 +1,23 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n <= 0) {
+        if(n == 0 || n < 0) {
             return false;
         }
-        return (n & (n - 1)) == 0;
+
+        bool found_one = false;
+        while(n != 0) {
+            if(n & 1 == 1) {
+                if(found_one) {
+                    return false;
+                }
+                else {
+                    found_one = true;
+                }
+            }
+            n = n >> 1;
+        }
+
+        return true;
     }
 };
