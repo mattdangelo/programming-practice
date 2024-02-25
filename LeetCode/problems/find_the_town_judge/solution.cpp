8dfc65b14047394d83bbdcdfe8c2877b7399304d
Matt D'Angelo <Astradus@users.noreply.github.com>
2024-02-25 12:55:54 +0000

    Sync LeetCode submission - Find the Town Judge (cpp)

--- LeetCode/problems/find_the_town_judge/solution.cpp
+++ LeetCode/problems/find_the_town_judge/solution.cpp
@@ -1,17 +1,27 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> counts(n, 0);
-        
-        for(std::vector<int> v : trust) {
-            counts[v[0] - 1]--;
-            counts[v[1] - 1]++;
+        // Special case - only 1 person
+        if(trust.size() == 0 && n == 1) {
+            return 1;
+        }
+    
+        std::vector<int> trust_factor(n, 0);
+
+        // Pass 1: Calculate the sum of relationships for each person, and filter out people
+        // who trust others
+        for(std::vector<int> relationship : trust) {
+            // Use 0 offsetting
+            trust_factor[relationship[1] - 1]++;
+            trust_factor[relationship[0] - 1]--;
         }
 
+        // Pass 2: Find person who matches the properties
         for(int i=0;i<n;i++) {
-            if(counts[i] == n - 1) {
+            if(trust_factor[i] == n - 1) {
+                // Convert to 1 indexed result
                 return i + 1;
-            }
+            } 
         }
 
         return -1;
