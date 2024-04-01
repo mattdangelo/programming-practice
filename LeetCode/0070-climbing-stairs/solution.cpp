9d08c88e48a54e3ccbdcafa2711f801d247cdb48
Matt D'Angelo <Astradus@users.noreply.github.com>
2024-04-01 06:06:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.9 MB (97.59%)

--- LeetCode/0070-climbing-stairs/solution.cpp
+++ LeetCode/0070-climbing-stairs/solution.cpp
@@ -1,19 +1,13 @@
 class Solution {
-    // Cache
     int cache[46] = {};
 public:
-    Solution() {
-        // Base cases (0, 1 and 2 steps)
-        for(int i=0;i<3;i++) {
-            cache[i] = i;
-        }
-    }
+    int climbStairs(int n) {
+        // Base cases
+        cache[0] = 1;
+        cache[1] = 1;
 
-    int climbStairs(int n) { 
-        int ctr = 3;
-        while(ctr <= n) {
-            cache[ctr] = cache[ctr - 1] + cache[ctr - 2];
-            ctr++;
+        for(int i=2;i<=n;i++) {
+            cache[i] = cache[i - 1] + cache[i - 2];
         }
 
         return cache[n];
