6fda16b32af1d71f0155e3e9005fcd0c004f8fe1
Matt D'Angelo <Astradus@users.noreply.github.com>
2024-04-11 02:19:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.3 MB (88.04%)

---
+++ Leetcode/2195-time-needed-to-buy-tickets/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int time_taken = 0;
+        while(tickets[k] > 0) {
+            // Go through everyone in the list
+            for(int i=0;i<tickets.size();i++) {
+                if(tickets[i] > 0) {
+                    // Buy them
+                    tickets[i]--;
+                    time_taken++;
+
+                    // Check if our condition is met
+                    if(i == k && tickets[k] == 0) {
+                        return time_taken;
+                    }
+                }
+            }
+        }
+
+        return time_taken;
+    }
+};
