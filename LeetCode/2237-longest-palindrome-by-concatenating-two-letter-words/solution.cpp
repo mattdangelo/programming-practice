1ffd0c7040ac4fa7da042e91f0af61d4cf1932ca
Matt D'Angelo <Astradus@users.noreply.github.com>
2025-05-25 03:43:38 +0000

    Sync LeetCode submission Runtime - 19 ms (91.77%), Memory - 171.5 MB (99.18%)

--- LeetCode/2237-longest-palindrome-by-concatenating-two-letter-words/solution.cpp
+++ LeetCode/2237-longest-palindrome-by-concatenating-two-letter-words/solution.cpp
@@ -1,58 +1,55 @@
 class Solution {
+    #define ASCII_OFFSET 97
 public:
     int longestPalindrome(vector<string>& words) {
-        std::unordered_map<std::string, int> counts;
-        for(std::string s : words) {
-            counts[s]++;
-        }
-
-        int count = 0;
+        int pal_length = 0;
 
         for(std::string s : words) {
-            if(counts[s] == 0) {
+            set_count(s[0], s[1], get_count(s[0], s[1]) + 1);
+
+            // If the count of rev_s is 0, there's no point trying to
+            // extend the palindrome
+            if(get_count(s[1], s[0]) == 0) {
                 continue;
             }
 
-            std::string reversed_s = rev(s);
-
             int num_of_pairs;
-            if(s == reversed_s) {
-                num_of_pairs = counts[s] / 2;
+            if(is_the_same_reversed(s)) {
+                num_of_pairs = get_count(s[0], s[1]) / 2;
             }
             else {
-                num_of_pairs = std::min(counts[s], counts[reversed_s]);
+                num_of_pairs = std::min(get_count(s[0], s[1]), get_count(s[1], s[0]));
             }
 
-            counts[s] -= num_of_pairs;
-            counts[reversed_s] -= num_of_pairs;
-            count += num_of_pairs;
+            set_count(s[0], s[1], get_count(s[0], s[1]) - num_of_pairs);
+            set_count(s[1], s[0], get_count(s[1], s[0]) - num_of_pairs);
+            pal_length += num_of_pairs;
         }
 
-        count *= 4;
+        pal_length *= 4;
 
-        for(std::pair<std::string, int> p : counts) {
-            if(p.second > 0 && isTheSameReversed(p.first)) {
-                count += 2;
+        for(std::string s : words) {
+            if(is_the_same_reversed(s) && get_count(s[0], s[1]) > 0) {
+                pal_length += 2;
                 break;
             }
         }
 
-        return count;
+        return pal_length;
     }
 
 private:
+    int counts[26][26] = { 0 };
 
-    inline std::string rev(std::string s) {
-        if(isTheSameReversed(s)) {
-            return s;
-        }
-
-        std::string r(1, s[1]);
-        r += s[0];
-        return r;
+    bool is_the_same_reversed(std::string& s) {
+        return s[1] == s[0];
     }
 
-    inline bool isTheSameReversed(std::string s) {
-        return s[1] == s[0];
+    int get_count(char first, char second) {
+        return counts[first - ASCII_OFFSET][second - ASCII_OFFSET];
+    }
+
+    void set_count(char first, char second, int count) {
+        counts[first - ASCII_OFFSET][second - ASCII_OFFSET] = count;
     }
 };
